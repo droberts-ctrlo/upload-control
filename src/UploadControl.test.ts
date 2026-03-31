968756ea55debc58dd7bef37b87f882151791a2d
Dave Roberts <d.roberts@ctrlo.com>
2026-03-31 15:39:21 +0100

    Initial version

---
+++ src/UploadControl.test.ts
@@ -0,0 +1,115 @@
+import { describe, it, expect, beforeEach, afterEach, jest } from '@jest/globals';
+import { Uploader } from './UploadControl';
+import type { XmlHttpRequestLike } from './UploadControl';
+
+class MockXhr implements XmlHttpRequestLike {
+    open: (method: string, url: string) => void = jest.fn();
+    onabort?: ((this: XMLHttpRequest, ev: ProgressEvent<EventTarget>) => any) = jest.fn();
+    onerror?: ((this: XMLHttpRequest, ev: ProgressEvent<EventTarget>) => any) = jest.fn();
+    onreadystatechange: ((this: XMLHttpRequest, ev: Event) => any) = jest.fn();
+    send: (data?: Document | XMLHttpRequestBodyInit | null | undefined) => void = jest.fn();
+    setRequestHeader: (header: string, value: string) => void = jest.fn();
+    readyState: number = 4;
+    status: number = 200;
+    responseText: string = JSON.stringify({error: 0});
+    upload = {
+        onprogress: jest.fn()
+    }
+}
+
+describe('UploadControl', () => {
+    let mockXhr: XmlHttpRequestLike | null;
+    let oldXMLHttpRequest: XMLHttpRequest;
+
+    beforeEach(() => {
+        mockXhr = new MockXhr();
+        oldXMLHttpRequest = <any>window.XMLHttpRequest; // eslint-disable-line @typescript-eslint/no-explicit-any
+        window.XMLHttpRequest = <any>(jest.fn(() => mockXhr)); // eslint-disable-line @typescript-eslint/no-explicit-any
+    });
+
+    afterEach(() => {
+        window.XMLHttpRequest = <any>oldXMLHttpRequest; // eslint-disable-line @typescript-eslint/no-explicit-any
+        mockXhr = null;
+    });
+
+    it('ensure mocks are correct', async () => {
+        const request = new XMLHttpRequest();
+        expect(request).toBeDefined();
+        expect(request.status).toBe(200);
+        expect(request.readyState).toBe(4);
+        expect(request.responseText).toBe(JSON.stringify({ error: 0 }));
+    });
+
+    it('should upload FormData using XMLHttpRequest', async () => {
+        const localMock = mockXhr!;
+        const { open, send } = localMock;
+        const url = 'http://localhost';
+        const method = 'POST';
+        const data = new FormData();
+        data.append('key', 'value');
+        const uploader = new Uploader(url, method);
+        const promise = uploader.upload(data);
+        setTimeout(localMock.onreadystatechange!, 1000);
+        const result = await promise;
+        expect(open).toHaveBeenCalledWith(method, url);
+        expect(send).toHaveBeenCalledWith(data);
+        expect(result).toEqual({ error: 0 });
+    });
+
+    it('should upload object using XMLHttpRequest', async () => {
+        const localMock = mockXhr!;
+        const { open, send } = localMock;
+        const url = 'http://localhost';
+        const method = 'POST';
+        const data = { key: 'value' };
+        const uploader = new Uploader(url, method);
+        const promise = uploader.upload(data);
+        setTimeout(localMock.onreadystatechange!, 1000);
+        const result = await promise;
+        expect(open).toHaveBeenCalledWith(method, url);
+        expect(send).toHaveBeenCalledWith(JSON.stringify(data));
+        expect(result).toEqual({ error: 0 });
+    });
+
+    it('should use a progress callback', async () => {
+        expect.assertions(2);
+        const localMock = mockXhr!;
+        const url = 'http://localhost';
+        const method = 'POST';
+        const data = { key: 'value' };
+        const uploader = new Uploader(url, method);
+        uploader.onProgress((loaded, total) => {
+            expect(loaded).toBe(1);
+            expect(total).toBe(2);
+        });
+        const promise = uploader.upload(data);
+        const ev: ProgressEvent = {
+            loaded: 1,
+            total: 2,
+            lengthComputable: true,
+        } as any;
+        setTimeout(() => {
+            localMock.upload?.onprogress && localMock.upload.onprogress(ev);
+        }, 500);
+        setTimeout(localMock.onreadystatechange!, 1500);
+        await promise;
+    });
+
+    it('should reject on error', async () => {
+        const localMock = mockXhr!;
+        const url = 'http://localhost';
+        const method = 'POST';
+        const data = { key: 'value' };
+        const uploader = new Uploader(url, method);
+        const promise = uploader.upload(data);
+        localMock.status = 500;
+        setTimeout(localMock.onreadystatechange!, 1000);
+        try {
+            await promise;
+            // Should not reach this point - so we fail the test by default!
+            expect(true).toBe(false);
+        } catch (e) {
+            expect(e).toBe(localMock.responseText);
+        }
+    });
+});
---
+++ src/UploadControl.ts
@@ -0,0 +1,96 @@
+/**
+ * Type to represent a function that is called when the upload progress changes
+ * @param loaded The number of bytes that have been uploaded
+ * @param total The total number of bytes to upload
+ */
+export type ProgressFunction = (loaded: number, total: number) => void;
+
+export type RequestMethod = "PUT"|"POST"|"GET"|"DELETE"|"PATCH";
+
+/**
+ * Type to represent an object similar to an XMLHttpRequest object
+ */
+export type XmlHttpRequestLike = {
+    open: (method: string, url: string) => void,
+    onabort?: ((this: XMLHttpRequest, ev: ProgressEvent<EventTarget>) => any) | null, // eslint-disable-line @typescript-eslint/no-explicit-any
+    onerror?: ((this: XMLHttpRequest, ev: ProgressEvent<EventTarget>) => any) | null, // eslint-disable-line @typescript-eslint/no-explicit-any
+    onreadystatechange: ((this: XMLHttpRequest, ev: Event) => any) | null, // eslint-disable-line @typescript-eslint/no-explicit-any
+    send: (data?: Document | XMLHttpRequestBodyInit | null | undefined) => void,
+    setRequestHeader: (header: string, value: string) => void,
+    readyState: number,
+    status: number,
+    responseText: string,
+    upload?: {
+        onprogress?: ((e: ProgressEvent) => void) | null
+    };
+};
+
+/**
+ * Upload data to a server endpoint
+ * @param url The endpoint to upload to
+ * @param data The form data to upload
+ * @param method The method to use, either POST, PUT, or GET
+ * @param onProgress A callback that is called when the upload progress changes
+ * @returns The JSON response from the server
+ */
+export async function upload<T = unknown>(url: string | URL, data?: FormData | object, method: RequestMethod = 'POST', onProgress: ProgressFunction = () => { }): Promise<T> {
+    const uploader = new Uploader(url, method);
+    uploader.onProgress(onProgress);
+    return uploader.upload(data);
+}
+
+/**
+ * Helper class to upload form data to a server endpoint
+ */
+export class Uploader {
+    private onProgressCallback?: ProgressFunction;
+    private url: string | URL;
+    private method: RequestMethod;
+
+    constructor(url: string | URL, method: RequestMethod) {
+        this.url = url;
+        this.method = method;
+    }
+
+    /**
+     * Set a callback that is called when the upload progress changes
+     * @param callback A callback that is called when the upload progress changes
+     */
+    onProgress(callback: ProgressFunction) {
+        this.onProgressCallback = callback;
+    }
+
+    /**
+     * Upload form data to a server endpoint
+     * @param data The form data or JSON object to upload
+     * @returns A promise that resolves to the JSON response from the server
+     */
+    async upload<T>(data?: FormData | object): Promise<T> {
+        return new Promise((resolve, reject) => {
+            const request: XmlHttpRequestLike & XMLHttpRequest = new XMLHttpRequest();
+            request.open(this.method, this.url.toString());
+            request.onabort = () => reject('aborted');
+            request.onerror = () => reject('error');
+            request.upload.onprogress = (e: ProgressEvent) => {
+                if(!e.lengthComputable) this.onProgressCallback?.(e.loaded, 0);
+                this.onProgressCallback?.(e.loaded, e.total);
+            }
+            request.onreadystatechange = () => {
+                if (request.readyState === 4) {
+                    if (request.status === 200) {
+                        resolve(JSON.parse(request.responseText));
+                    } else {
+                        reject(request.responseText);
+                    }
+                }
+            };
+
+            if (data instanceof FormData) {
+                request.send(data);
+            } else {
+                request.setRequestHeader('Content-Type', 'application/json');
+                request.send(JSON.stringify(data));
+            }
+        });
+    }
+}
